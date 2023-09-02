0e0d05ac17330072ed25fcd4a908fd91d45f5514
Noelia Soledad <103439118+LNsoledad@users.noreply.github.com>
2023-09-01 22:36:33 -0300

    Add files via upload

---
+++ Practico5/Practico5/Form1.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form reemplaza a Dispose para limpiar la lista de componentes.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Requerido por el Diseñador de Windows Forms
+	Private components As System.ComponentModel.IContainer
+
+	'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+	'Se puede modificar usando el Diseñador de Windows Forms.  
+	'No lo modifique con el editor de código.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TFoto = New System.Windows.Forms.TextBox()
+        Me.TSaldo = New System.Windows.Forms.TextBox()
+        Me.TApellido = New System.Windows.Forms.TextBox()
+        Me.TNombre = New System.Windows.Forms.TextBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.BFoto = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Papyrus", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label1.Location = New System.Drawing.Point(29, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 34)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Menu
+        Me.Panel1.BackgroundImage = Global.Practico5.My.Resources.Resources._0c21f2fefe38403d1497dead1019f63a
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.TFoto)
+        Me.Panel1.Controls.Add(Me.TSaldo)
+        Me.Panel1.Controls.Add(Me.TApellido)
+        Me.Panel1.Controls.Add(Me.TNombre)
+        Me.Panel1.Controls.Add(Me.RadioButton2)
+        Me.Panel1.Controls.Add(Me.RadioButton1)
+        Me.Panel1.Controls.Add(Me.DateTimePicker1)
+        Me.Panel1.Controls.Add(Me.BFoto)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.ForeColor = System.Drawing.Color.White
+        Me.Panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Panel1.Location = New System.Drawing.Point(66, 37)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(407, 322)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Image = Global.Practico5.My.Resources.Resources._678092_sign_add_128
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(164, 274)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 36)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Agregar"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TFoto
+        '
+        Me.TFoto.Location = New System.Drawing.Point(151, 248)
+        Me.TFoto.Name = "TFoto"
+        Me.TFoto.Size = New System.Drawing.Size(240, 20)
+        Me.TFoto.TabIndex = 13
+        '
+        'TSaldo
+        '
+        Me.TSaldo.Location = New System.Drawing.Point(151, 215)
+        Me.TSaldo.Name = "TSaldo"
+        Me.TSaldo.Size = New System.Drawing.Size(158, 20)
+        Me.TSaldo.TabIndex = 12
+        '
+        'TApellido
+        '
+        Me.TApellido.Location = New System.Drawing.Point(151, 90)
+        Me.TApellido.Name = "TApellido"
+        Me.TApellido.Size = New System.Drawing.Size(158, 20)
+        Me.TApellido.TabIndex = 11
+        '
+        'TNombre
+        '
+        Me.TNombre.Location = New System.Drawing.Point(151, 40)
+        Me.TNombre.Name = "TNombre"
+        Me.TNombre.Size = New System.Drawing.Size(158, 20)
+        Me.TNombre.TabIndex = 10
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton2.Font = New System.Drawing.Font("Papyrus", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(257, 166)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(70, 29)
+        Me.RadioButton2.TabIndex = 9
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Mujer"
+        Me.RadioButton2.UseVisualStyleBackColor = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton1.Font = New System.Drawing.Font("Papyrus", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(151, 166)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(89, 29)
+        Me.RadioButton1.TabIndex = 8
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Hombre"
+        Me.RadioButton1.UseVisualStyleBackColor = False
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(257, 132)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(95, 20)
+        Me.DateTimePicker1.TabIndex = 6
+        '
+        'BFoto
+        '
+        Me.BFoto.ForeColor = System.Drawing.Color.Black
+        Me.BFoto.Location = New System.Drawing.Point(35, 245)
+        Me.BFoto.Name = "BFoto"
+        Me.BFoto.Size = New System.Drawing.Size(75, 23)
+        Me.BFoto.TabIndex = 5
+        Me.BFoto.Text = "Foto"
+        Me.BFoto.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Papyrus", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label5.Location = New System.Drawing.Point(29, 207)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 34)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Saldo:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Papyrus", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label4.Location = New System.Drawing.Point(29, 161)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 34)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Sexo:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Papyrus", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label3.Location = New System.Drawing.Point(29, 127)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(222, 34)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Fecha de Nacimiento:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Papyrus", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label2.Location = New System.Drawing.Point(29, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 34)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Apellido:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox1.Location = New System.Drawing.Point(534, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(246, 322)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 365)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(785, 150)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Column1
+        '
+        Me.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
+        Me.Column1.HeaderText = "Apellido"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 69
+        '
+        'Column2
+        '
+        Me.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
+        Me.Column2.HeaderText = "Nombre"
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 69
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Fecha de Nacimiento"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Sexo"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Eliminar"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        Me.Column5.Text = "Eliminar"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Saldo"
+        Me.Column6.Name = "Column6"
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Foto"
+        Me.Column7.Name = "Column7"
+        Me.Column7.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Column7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "Ruta"
+        Me.Column8.Name = "Column8"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Practico5.My.Resources.Resources.fondo_negro
+        Me.ClientSize = New System.Drawing.Size(877, 539)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Formulario con Grid"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+	Friend WithEvents Panel1 As Panel
+	Friend WithEvents BFoto As Button
+	Friend WithEvents Label5 As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents RadioButton1 As RadioButton
+	Friend WithEvents DateTimePicker1 As DateTimePicker
+	Friend WithEvents Button2 As Button
+	Friend WithEvents TFoto As TextBox
+	Friend WithEvents TSaldo As TextBox
+	Friend WithEvents TApellido As TextBox
+	Friend WithEvents TNombre As TextBox
+	Friend WithEvents RadioButton2 As RadioButton
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents DataGridView1 As DataGridView
+	Friend WithEvents Column1 As DataGridViewTextBoxColumn
+	Friend WithEvents Column2 As DataGridViewTextBoxColumn
+	Friend WithEvents Column3 As DataGridViewTextBoxColumn
+	Friend WithEvents Column4 As DataGridViewTextBoxColumn
+	Friend WithEvents Column5 As DataGridViewButtonColumn
+	Friend WithEvents Column6 As DataGridViewTextBoxColumn
+	Friend WithEvents Column7 As DataGridViewImageColumn
+	Friend WithEvents Column8 As DataGridViewTextBoxColumn
+End Class
---
+++ Practico5/Practico5/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+	Dim RB As String
+
+	Private Sub BFoto_Click(sender As Object, e As EventArgs) Handles BFoto.Click
+		Dim OpenFileDialog1 As New OpenFileDialog
+
+		OpenFileDialog1.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+		OpenFileDialog1.Filter = "Archivos Imagenes|*.jpg|Archivos Imagenes|*.bmp|Archivos
+Imagenes|*.png"
+		If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+			PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+			Tfoto.Text = OpenFileDialog1.FileName
+			MsgBox(OpenFileDialog1.FileName)
+
+		End If
+	End Sub
+
+	Private Sub TNombre_TextChanged(sender As Object, e As EventArgs) Handles TNombre.TextChanged
+		Dim i As Integer
+		TNombre.Text = StrConv(TNombre.Text, VbStrConv.ProperCase)
+		i = Len(TNombre.Text)
+		TNombre.SelectionStart = i
+	End Sub
+
+	Private Sub TApellido_TextChanged(sender As Object, e As EventArgs) Handles TApellido.TextChanged
+		Dim i As Integer
+		TApellido.Text = StrConv(TApellido.Text, VbStrConv.ProperCase)
+		i = Len(TApellido.Text)
+		TApellido.SelectionStart = i
+	End Sub
+
+	Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+
+		If RadioButton1.Checked = True Then
+
+			RB = "Hombre"
+
+		Else
+
+			RB = "Mujer"
+
+		End If
+
+	End Sub
+
+	Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+		If RadioButton2.Checked = True Then
+
+			RB = "Mujer"
+
+		Else
+
+			RB = "Hombre"
+
+		End If
+	End Sub
+
+
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		Dim resultado As MsgBoxResult
+
+		If (TNombre.Text.Length = 0 Or TApellido.Text.Length = 0 Or TSaldo.Text.Length = 0 Or TFoto.Text.Length = 0) Then
+
+			MsgBox("Debe Completar todos los campos", vbCritical, "ERROR")
+
+		Else
+			resultado = MessageBox.Show("Seguro que desea guardar?", "Confirmar Inserción",
+									 MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+			If resultado = vbYes Then
+
+				DataGridView1.RowTemplate.Height = 50
+				DataGridView1.Rows.Add(TApellido.Text, TNombre.Text, DateTimePicker1.Text, RB, "Eliminar", TSaldo.Text, PictureBox1.Image, TFoto.Text)
+				Column1.DefaultCellStyle.Font = New Font("Monotype Corsiva", 15, FontStyle.Italic)
+				Column2.DefaultCellStyle.Font = New Font("Monotype Corsiva", 15, FontStyle.Italic)
+				For Each r As DataGridViewRow In DataGridView1.Rows
+
+					If r.Cells("Column6").Value < 50 Then
+
+						r.DefaultCellStyle.BackColor = Color.Red
+
+					End If
+
+				Next
+				Column7.ImageLayout = DataGridViewImageCellLayout.Stretch
+			End If
+		End If
+
+	End Sub
+
+	Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+		Dim senderGrid = DirectCast(sender, DataGridView)
+		If TypeOf senderGrid.Columns(e.ColumnIndex) Is DataGridViewButtonColumn Then
+			Dim ask As MsgBoxResult = MsgBox("Desea elminar este registro?", MsgBoxStyle.YesNo)
+			If ask = MsgBoxResult.Yes Then
+				DataGridView1.Rows.Remove(DataGridView1.CurrentRow)
+				MsgBox("se elimino correctamente", MsgBoxStyle.OkOnly, "Eliminar")
+			End If
+		End If
+	End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+    End Sub
+End Class
